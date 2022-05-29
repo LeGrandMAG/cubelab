e5cf4088ef966ff547e2b8112985375f3589a954
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-05-29 15:58:10 +0900

    staticfile

--- .history/wepo/settings/base_20220529155722.py
+++ .history/wepo/settings/base_20220529155722.py
@@ -142,9 +142,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']"""
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = BASE_DIR / 'media'
